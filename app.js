0b70308978c42b2b8c2b8c3b1e363e0e85b577da
A-Singh15 <70713574+A-Singh15@users.noreply.github.com>
2020-11-23 19:19:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
-const express        = require("express"),
-      app            = express(),
-      bodyParser     = require("body-parser"),
-      mongoose       = require("mongoose"),
-      helmet         = require("helmet"),
-      flash          = require("connect-flash"),
-      session        = require("express-session"),
-      moment         = require("moment"),
-      passport       = require("passport"),
-      LocalStrategy  = require("passport-local"),
-      methodOverride = require("method-override"),
-      User           = require("./models/user");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
+    LocalStrategy = require("passport-local"),
+    flash        = require("connect-flash"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    session = require("express-session"),
+    seedDB      = require("./seeds"),
+    methodOverride = require("method-override");
+// configure dotenv
+require('dotenv').load();
 
-// requiring routes     
-const indexRoute      = require("./routes/index"),
-      campgroundRoute = require("./routes/campgrounds"),
-      commentRoute    = require("./routes/comments"),
-      userRoute       = require("./routes/user"),
-      passwordRoute   = require("./routes/password");
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+// assign mongoose promise library and connect to database
+mongoose.Promise = global.Promise;
 
-// connect to the DB
-let url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v13"; // fallback in case global var not working
-mongoose.connect(url, {useMongoClient: true});
+mongoose.connect("mongodb://localhost:27017/csc317", {useNewUrlParser: true});
 
-app.set("view engine", "ejs");
-app.use(helmet());
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
-app.locals.moment = moment; // create local variable available for the application
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
+//require moment
+app.locals.moment = require('moment');
+// seedDB(); //seed the database
 
-//passport configuration
-app.use(session({
-  secret: process.env.SESSIONSECRET,
-  resave: false,
-  saveUninitialized: false
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
 }));
+
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// pass currentUser to all routes
-app.use((req, res, next) => {
-  res.locals.currentUser = req.user; // req.user is an authenticated user
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
-  next();
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
 });
 
-// use routes
-app.use("/", indexRoute);
-app.use("/campgrounds", campgroundRoute);
-app.use("/campgrounds/:id/comments", commentRoute);
-app.use("/users", userRoute);
-app.use("/", passwordRoute);
 
-app.listen(process.env.PORT, process.env.IP, () => console.log("The YelpCamp Server Has Started!"));
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(3000, process.env.IP, function() {
+    console.log("The Server Has Started!");
+});
